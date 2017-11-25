3dd773826c1a22079fa52ebe603ba3adac1c7b96
Kundan <kkishor@outlook.com>
2017-11-26 00:30:59 +0400

    Added Web Module

---
+++ otrs-restaurant-web/src/main/resources/static/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name otrsWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the otrsWebApp
+ */
+angular.module('otrsWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ otrs-restaurant-web/src/main/resources/static/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name otrsWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the otrsWebApp
+ */
+angular.module('otrsWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
