3f1ad358fc9bf1edd591ece9acdccf5cec71c0b6
Kundan <kkishor@outlook.com>
2017-11-28 00:20:30 +0400

    Added Email functionality

--- otrs-booking-service/src/main/java/com/otrs/restaurant/service/impl/BookingServiceImpl.java
+++ otrs-booking-service/src/main/java/com/otrs/restaurant/service/impl/BookingServiceImpl.java
@@ -3,8 +3,16 @@
  */
 package com.otrs.restaurant.service.impl;
 
-import java.util.Calendar;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +48,7 @@ public class BookingServiceImpl implements BookingService {
 	public String saveBooking(Booking booking) {
 		try {
 			bookingRepository.save(booking);
+			sendBookingEmail(booking.getUserId(), booking.getPrice(), booking.getRestaurantName(), booking.getNoOfBookedTables());
 		} catch (Exception e) {
 			if (e instanceof DataIntegrityViolationException) {
 				logger.error(e.getMessage());
@@ -70,6 +79,41 @@ public class BookingServiceImpl implements BookingService {
 			return "Booking Cleared";
 		}
 		return "Error";
+	}
+
+	private void sendBookingEmail(String emailId, Double amount, String restaurantName, Integer tables) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("kundanmca123@gmail.com", "pepfpeeyovdwersh");
+			}
+		});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("kundanmca123@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailId));
+			message.setSubject("Your booking Details");
+			message.setText("Dear User," 
+					+ "\nThis is a confirmation email for your booking." 
+					+ "\nRestaurant Name : " + restaurantName
+					+ "\nTables Booked : " + tables
+					+ "\nAmount Payable : " + amount
+					+ "\n\nThank you for booking with OTRS."
+					+ "\nRegards,\nOTRS");
+
+			Transport.send(message);
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
